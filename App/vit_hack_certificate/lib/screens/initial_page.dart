4084a1cd4c551c35150e07f513370b1d2fa01608
Brajesh Kumar Roy <57515174+bkrroy@users.noreply.github.com>
2020-10-09 19:57:21 +0530

    added buttons

--- App/vit_hack_certificate/lib/screens/initial_page.dart
+++ App/vit_hack_certificate/lib/screens/initial_page.dart
@@ -7,17 +7,73 @@ class LoginOrRegister extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kAppBackgroundColour,
-      body: Container(
-          height: MediaQuery.of(context)
-              .size
-              .height, // to make the container height equal to height of the device
-          width: MediaQuery.of(context)
-              .size
-              .width, // to make the width equal to the width of the device
-          child: Image.asset(
-            'images/login.jpeg', // importing the image for background
-            fit: BoxFit.cover, // To cover the entire screen
-          )),
+      body: Stack(
+        children: [
+          // Container for the background image
+          Container(
+            height: MediaQuery.of(context)
+                .size
+                .height, // to make the container height equal to height of the device
+            width: MediaQuery.of(context)
+                .size
+                .width, // to make the width equal to the width of the device
+            child: Image.asset(
+              'images/login.jpeg', // importing the image for background
+              fit: BoxFit.cover, // To cover the entire screen
+            ),
+          ),
+          // Column for adding appName, login and Registration
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // Container to store name of the app
+                Container(
+                  child: Text(
+                    'HACK_CERTIFICATE',
+                    style:
+                        TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                // Container to have the login button
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.1,
+                ),
+                LoginOrRegisterButton(text: 'Login'),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.02,
+                ),
+                LoginOrRegisterButton(text: 'Register'),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class LoginOrRegisterButton extends StatelessWidget {
+  LoginOrRegisterButton({@required this.text});
+
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.08,
+      width: MediaQuery.of(context).size.width * 0.8,
+      child: Center(
+        child: Text(
+          text,
+          style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.w600),
+        ),
+      ),
+      decoration: BoxDecoration(
+        color: kLoginButtonColour,
+        borderRadius: BorderRadius.all(
+          Radius.circular(30.0),
+        ),
+      ),
     );
   }
 }
